d222382024ea14e7b9661c6f33b39a765883762b
mkalchev <mihail_kalchev@abv.bg>
2023-05-23 22:15:02 +0300

    Delete 01_first_steps_in_coding_exercise_08.01.2023 directory

--- 01_first_steps_in_coding_exercise_08.01.2023/01usd_to_bgn.py
+++
@@ -1,5 +0,0 @@
-usd = float(input())
-bgn = 1.79549
-convert_usd_to_bgn = usd * bgn
-
-print(convert_usd_to_bgn)
--- 01_first_steps_in_coding_exercise_08.01.2023/02_radians_to_degrees.py
+++
@@ -1,7 +0,0 @@
-from math import pi
-#pi трябва да се импортне
-
-radians = float(input())
-degrees = radians * 180 / pi
-
-print(degrees)
